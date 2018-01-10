7b5365a4dd6eb6294e52ac35305b0af3218ba37f
Abel Molnar - mac <abel.capable@hotmail.com>
2018-01-10 02:25:06 +0100

    Initial Commit

---
+++ foovisor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  foovisor
+//
+//  Created by Abel Molnar on 2018. 01. 10..
+//  Copyright © 2018. Abel Molnar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
